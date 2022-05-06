55f07295c7d79af163663bfca746e792b929ef63
GuoYuanLi1 <99737692+GuoYuanLi1@users.noreply.github.com>
2022-05-05 20:03:17 -0400

    Add files via upload

---
+++ Cargo/Cargo.js
@@ -0,0 +1,256 @@
+Module.register("Cargo", {
+  defaults: {
+    Openpage: false,
+    lockOpen: false,
+  },
+  
+
+  start: function () {
+    Log.info(this.name + " has started...");
+    this.sendSocketNotification("CONFIG", this.config);
+  },
+
+  getStyles: function () {
+    return [this.file("css/cargo.css"), "font-awesome.css"];
+  },
+
+  // Load translations files
+  getTranslations: function () {
+    return {
+      en: "translations/en.json",
+      nb: "translations/nb.json",
+    };
+  },
+
+  createContainerDiv: function () {
+    const containerDiv = document.createElement("div");
+    containerDiv.className = "st-container";
+
+    return containerDiv;
+  },
+
+  
+  
+  toggleReceiveMenu: function () {
+    const menuToggleDiv = document.getElementById("st-receivemenu-toggle")
+    menuToggleDiv.classList.toggle('show');
+
+    const mainMenuDiv = document.getElementById("st-receivemain-menu")
+    mainMenuDiv.classList.toggle('show')
+  },
+  
+
+  createReceiveButtonDiv: function () {
+    const receiveButtonDiv = document.createElement("div");
+    receiveButtonDiv.className = "st-container__receive-button";
+    receiveButtonDiv.id = "st-receivemenu-toggle";
+
+    receiveButtonDiv.innerHTML = "<span class='fa fa-unlock fa-3x'></span>"
+        + "<br>" + this.translate('');
+    receiveButtonDiv.addEventListener("click", () => {
+      this.toggleReceiveMenu();
+      //this.sendSocketNotification("CARGO_START",2);
+    });
+
+    return receiveButtonDiv;
+  },
+  
+  createGetButton: function () {
+    const getButtonItem = document.createElement("li");
+    getButtonItem.innerHTML = "<span class='fa fa-check fa-3x'></span>"
+        + "<br>" + this.translate('Confirm & Exit');
+    getButtonItem.className = "li-t";
+    getButtonItem.addEventListener("click", () => {
+      this.config.Openpage = false;
+      // this.sendSocketNotification("assert");
+      this.sendSocketNotification("CARGO_START",2);
+      
+      this.toggleReceiveMenu();
+    });
+
+    return getButtonItem;
+  },
+  
+   createReceiveHeader: function () {
+    var header = document.createElement("li");
+    header.innerText = "Cargo Retrieval";
+    header.style.fontSize = "75pt";
+    header.style.lineHeight = "75pt";
+
+    return header;
+  },
+
+  createReceiveText: function () {
+    var text = document.createElement("li");
+    text.innerText = "Please place your finger on the fingerprint sensor\n to retrive your cargo";
+    text.style.fontSize = "40pt";
+    text.style.lineHeight = "40pt";
+   
+
+    return text;
+
+  },
+
+  toggleSideMenu: function () {
+    const menuToggleDiv = document.getElementById("st-menu-toggle")
+    menuToggleDiv.classList.toggle('show');
+
+    const mainMenuDiv = document.getElementById("st-main-menu")
+    mainMenuDiv.classList.toggle('show')
+  },
+
+  createMenuToggleButtonDiv: function () {
+    //const menuToggleButtonItem = document.createElement("li");
+    const menuToggleButtonDiv = document.createElement("div");
+    menuToggleButtonDiv.id = "st-menu-toggle";
+    
+    menuToggleButtonDiv.innerHTML = "<span class='fa fa-truck fa-3x'></span>"
+        + "<br>" + this.translate('');
+    menuToggleButtonDiv.className = "st-container__menu-toggle";
+
+    menuToggleButtonDiv.addEventListener("click", () => {
+      this.sendSocketNotification("assert");
+      Openpage = true;
+      this.toggleSideMenu();
+      });
+
+    //return menuToggleButtonItem;
+    return menuToggleButtonDiv;
+  },
+
+  createCloseButton: function () {
+    const closeButtonItem = document.createElement("li");
+    closeButtonItem.innerHTML = "<span class='fa fa-times-circle fa-3x'></span>"
+        + "<br>" + this.translate('CLOSE');
+    closeButtonItem.className = "li-t";
+    closeButtonItem.addEventListener("click", () => {
+      this.config.Openpage = false;
+      this.sendSocketNotification("assert");
+      this.toggleSideMenu();
+    });
+
+    return closeButtonItem;
+  },
+  
+  
+   createConfirmButton: function () {
+    const confrimButtonItem = document.createElement("li");
+    confrimButtonItem.innerHTML = "<span class='fa fa-check fa-3x'></span>"
+        + "<br>" + this.translate('Confirm & Exit');
+    confrimButtonItem.className = "li-t";
+    confrimButtonItem.addEventListener("click", () => {
+      this.config.Openpage = false;
+      // this.sendSocketNotification("assert");
+      this.sendSocketNotification("CARGO_START",1);
+      
+      this.toggleSideMenu();
+    });
+
+    return confrimButtonItem;
+  },
+  
+
+
+  createIFrame: function () {
+    iframe = document.createElement("IFRAME");
+    iframe.width = "99.5%";
+    iframe.height = "950px"; // 630px for 13 inch
+    iframe.scrolling = "no";
+    iframe.src = "http://139.147.199.252:8081/"; 
+    
+    return iframe;
+  },
+  
+  
+  createReceiveMenuDiv: function () {
+    const mainMenuDiv = document.createElement("div");
+    mainMenuDiv.className = "st-container__receivemain-menu";
+    mainMenuDiv.id = "st-receivemain-menu";
+    
+    
+    const ReceiveHeader = this.createReceiveHeader();
+    const ReceiveText = this.createReceiveText();
+    const GetButton = this.createGetButton();
+    
+    const buttonList = document.createElement("ul");
+    buttonList.appendChild(ReceiveHeader);
+    buttonList.appendChild(ReceiveText);
+    buttonList.appendChild(GetButton);
+    
+    mainMenuDiv.appendChild(buttonList);
+
+    
+    return mainMenuDiv;
+  },
+  
+  
+  createMainMenuDiv: function () {
+    const mainMenuDiv = document.createElement("div");
+    mainMenuDiv.className = "st-container__main-menu";
+    mainMenuDiv.id = "st-main-menu";
+    
+    // const closeButton = this.createCloseButton();
+    iFrame = this.createIFrame();
+    const confirmButton = this.createConfirmButton();
+    
+
+    const buttonList = document.createElement("ul");
+    
+    buttonList.appendChild(confirmButton);
+    buttonList.appendChild(iFrame);
+    
+    
+    mainMenuDiv.appendChild(buttonList);
+
+    return mainMenuDiv;
+  },
+
+  getDom: function () {
+    // Initial standby state
+    document.body.className = "st-standby show";
+    
+    ///this.sendSocketNotification("deassert");
+
+    const container = this.createContainerDiv();
+
+    const ReceiveButton = this.createReceiveButtonDiv();
+    container.appendChild(ReceiveButton);
+
+    const menuToggleButton = this.createMenuToggleButtonDiv();
+    container.appendChild(menuToggleButton);
+
+    const mainMenu = this.createMainMenuDiv();
+    document.body.appendChild(mainMenu);
+    
+    const receiveMenu = this.createReceiveMenuDiv();
+    document.body.appendChild(receiveMenu);
+    
+
+    return container;
+  },
+  
+
+  notificationReceived: function (notification, payload, sender) {
+     switch(notification) {
+        case "DOM_OBJECTS_CREATED":
+          var timer = setInterval(()=>{
+          this.sendSocketNotification("CARGO_START",0);
+        }, 200)
+        break
+      }
+  },
+
+  // Recieve notification from sockets via nodehelper.js
+  socketNotificationReceived: function (notification) {
+    switch(notification) {
+      case "RETURN_NOW":
+        if(Openpage) {
+          this.toggleSideMenu();
+          Openpage = false;
+        }
+        break;
+        
+    }
+  },
+
+});
---
+++ Cargo/node_helper.js
@@ -0,0 +1,181 @@
+/* Magic Mirror
+ * Node Helper: MMM-SmartTouch
+ *
+ * By SmartBuilds.io - Pratik and Eben
+ * https://smartbuilds.io
+ * MIT Licensed.
+ */
+ 
+var util = require("util");
+const NodeHelper = require("node_helper")
+
+
+module.exports = NodeHelper.create({
+  start: function () {
+    this.started = false;
+    this.config = {};
+  },
+
+  socketNotificationReceived: function (notification, payload) {
+    if (notification === 'CONFIG') {
+      if (!this.started) {
+        this.config = payload;
+        this.started = true;
+        console.log("cargo module has started")
+        //console.log("proceed")
+        this.sendSocketNotification("SHUTIT", payload);
+      }
+    }
+    
+
+    if (notification === 'assert') {
+      var fs = require('fs');
+     
+      fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/interact_in.txt", "1", function (err) {
+           if(err) {
+             console.log("Error occured: write file failed")
+           }else{
+             console.log("Clicked!")
+           }
+        });
+        
+        //delay x millisecond and then deassert click signal
+        setTimeout(function(){ 
+          fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/interact_in.txt", "0", function (err) {
+           if(err) {
+             console.log("Error occured: write file failed")
+           }else{
+             console.log("Not Clicked!")
+           }
+          });
+        }, 2000);
+          
+    }
+    
+    
+     if (notification === 'CARGO_START') {
+        var fs = require('fs');
+      // var return_home = '';
+     
+        fs.readFile("/home/pi/Desktop/MagicMirror/modules/Cargo/interact_end_out.txt", function (err, data) {
+           if(err) console.log("Error occured: read file failed")
+           return_home = data.toString().replace(/\s+/g, '');
+           // console.log("Return home: " + return_home);
+           
+        });
+    
+        fs.readFile("/home/pi/Desktop/MagicMirror/modules/Cargo/name.txt", function (err, data) {
+              if(err) console.log("Error occured: read file failed")
+              name_web = data.toString().replace(/\s+/g, '');
+              //console.log("Confirm: " + confirm);
+            });
+            
+        fs.readFile("/home/pi/Desktop/MagicMirror/modules/Cargo/fingerprint.txt", function (err, data) {
+              if(err) console.log("Error occured: read file failed")
+              name_finger = data.toString().replace(/\s+/g, '');
+              //console.log("Confirm: " + confirm);
+            });
+            
+         if(name_web === name_finger) {
+              // Open lock
+              fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/lock.txt", "1", function (err) {
+                if(err) {
+                  console.log("Error occured: write file failed")
+                }else{
+                  console.log("Name matched! Open lock!")
+                }
+              });
+            }
+        
+        
+        if(return_home === '1') {
+          // console.log("Return home screen");
+          this.sendSocketNotification("RETURN_NOW");
+        }
+        if(payload === 0) {
+            fs.readFile("/home/pi/Desktop/MagicMirror/modules/Cargo/delivery.txt", function (err, data) {
+              if(err) console.log("Error occured: read file failed")
+              delivery = data.toString().replace(/\s+/g, '').charAt(0);
+              // console.log("Return home: " + return_home);
+            });
+        
+            fs.readFile("/home/pi/Desktop/MagicMirror/modules/Cargo/confirm.txt", function (err, data) {
+              if(err) console.log("Error occured: read file failed")
+              confirm = data.toString().replace(/\s+/g, '');
+              //console.log("Confirm: " + confirm);
+            });
+        
+        
+            if(confirm === '1' && delivery === '1') {
+              // Open lock
+              fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/lock.txt", "1", function (err) {
+              if(err) {
+                console.log("Error occured: write file failed")
+              }else{
+                console.log("Open lock!")
+              }
+              });
+              // this.sendSocketNotification("LOCK_OPEN");
+            }
+        }else if(payload === 1) {
+            // Deassert confirm
+            fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/confirm.txt", "0", function (err) {
+              if(err) {
+                console.log("Error occured: write file failed")
+              }else{
+                console.log("Deassert confirm")
+              }
+            });
+        
+            // Close Lock
+            fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/lock.txt", "0", function (err) {
+              if(err) {
+                console.log("Error occured: write file failed")
+              }else{
+                console.log("Close Lock!")
+                confirm = '0';
+                //console.log("Confirm: " + confirm);
+              }
+            });
+          }else if(payload === 2) {
+             // Close Lock
+              fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/lock.txt", "0", function (err) {
+                  if(err) {
+                    console.log("Error occured: write file failed")
+                  }else{
+                    console.log("User received cargo! Close Lock!")
+                    name_finger = '111';
+                    name_web = "222";
+                    //console.log("Confirm: " + confirm);
+                  }
+                });
+                
+            fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/name.txt", "111", function (err) {
+              if(err) {
+                console.log("Error occured: write file failed")
+              }else{
+                name_web = "222";
+                console.log("Erase web name")
+              }
+            });
+        
+            // Close Lock
+            fs.writeFile("/home/pi/Desktop/MagicMirror/modules/Cargo/fingerprint.txt", "222", function (err) {
+              if(err) {
+                console.log("Error occured: write file failed")
+              }else{
+                name_finger = '111';
+                console.log("Erase fingerprint name")
+                //console.log("Confirm: " + confirm);
+              }
+            });
+          
+          }
+        
+    }
+    
+    
+   
+    
+  },
+});
